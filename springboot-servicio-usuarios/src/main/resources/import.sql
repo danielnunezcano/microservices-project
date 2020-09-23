75c2b6c86aa6a61942eddbf67e95f4ffb1eb6020
Daniel Núñez Cano <danielnunezcano@gmail.com>
2020-09-23 23:44:13 +0200

    Avance modulo 7.8

---
+++ springboot-servicio-usuarios/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO 'usuarios' (username, password, enabled, nombre, apellido, email) VALUES ('andres','12345',1,'Andres','Guzman','profesor@bolsadeideas.com');
+INSERT INTO 'usuarios' (username, password, enabled, nombre, apellido, email) VALUES ('admin','12345',1,'John','Doe','jhon.doe@bolsadeideas.com');
--- starts/zuul.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# -*- ENCODING: UTF-8 -*-
-java -jar springboot-servicio-zuul-server-0.0.1-SNAPSHOT.jar
-
