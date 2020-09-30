9f85b74796dc9a632c1efcee002c3760180eb505
Daniel Núñez Cano <danielnunezcano@gmail.com>
2020-09-30 21:46:04 +0200

    Avance modulo 7.13

--- springboot-servicio-oauth/src/test/java/com/formacionbdi/springboot/app/oauth/SpringbootServicioOauthApplicationTests.java
+++ springboot-servicio-oauth/src/test/java/com/formacionbdi/springboot/app/oauth/SpringbootServicioOauthApplicationTests.java
@@ -1,10 +1,10 @@
-package com.formacionbdi.springboot.app.usuarios.commons;
+package com.formacionbdi.springboot.app.oauth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootServicioUsuariosCommonsApplicationTests {
+class SpringbootServicioOauthApplicationTests {
 
 	@Test
 	void contextLoads() {
